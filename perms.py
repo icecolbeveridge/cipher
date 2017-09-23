e564dfc0ba5b1f6a8edc4229ccfa61d676396c67
Colin Beveridge <colin@flyingcoloursmaths.co.uk>
2017-09-23 21:39:54 +0100

    Permutations plan

---
+++ perms.py
@@ -0,0 +1,5 @@
+# permutation / genetics
+
+# populations comprise N pairs of permutations of AA-ZZ.
+# two random pairs (p, q) selected to breed, removed
+# best two of p, q, pq*p and qp*q added to population
